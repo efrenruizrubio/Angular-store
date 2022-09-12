f92f9f97b7fe333597f1ef1aa693b5147ae49431
= <=>
2022-09-12 11:09:54 -0500

    :sparkles: [ADD]: Event Bindings (scroll and keyup)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title: string = 'angular-store';
@@ -18,5 +18,13 @@ export class AppComponent {
   }
   incrementAge() {
     this.person.age++;
+  }
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
   }
 }
